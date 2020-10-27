5085147811974cc8dbfa202aa6830b888a186191
Schatfield <dgoldmine@icloud.com>
2020-10-27 12:32:50 -0500

    debugging react import statement erorrs

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,25 +1,28 @@
+
 import React, { Component } from 'react';
-//import { Link, withRouter } from "react-router-dom"
+import { Link, Router, withRouter } from "react-router-dom";
 //import './NavBar.css'
 class NavBar extends Component {
     render() {
       return (
+      
         <header>
           <nav> 
             <div className="nav-bar">
               <ul className="container">
-                <li><Link className="nav-link" to="/"></Link></li>
-                <li><Link className="nav-link" to="/"></Link></li>
-                <li><Link className="nav-link" to="/"></Link></li>
-                <li><Link className="nav-link" to="/"></Link></li>
-                <li><Link className="nav-link" to="/"></Link></li>
+                <li><Link className="nav-link" to="/"></Link></li> 
+                <li><Link className="nav-link" to="/">/ ABOUT /</Link></li>
+                <li><Link className="nav-link" to="/"></Link>/ PROJECTS /</li>
+                <li><Link className="nav-link" to="/"></Link>/ CONTACT /</li>
+                
 
               </ul>
             </div>
           </nav>
         </header>
+     
       );
     }
   }
 
-  export default NavBar;
+  export default withRouter(NavBar)
